9d0bea71f933f1a1397d5b9d2cfb2c4842941dd4
ShalyneKerubo <shalyne.kerubo@gmail.com>
2023-09-07 17:59:31 -0700

    A C program that prints a string using the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A simple C program that uses the puts() function to print a string to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
