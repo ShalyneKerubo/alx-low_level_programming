a83679b72b4a178126bae477330df347cbc83666
ShalyneKerubo <shalyne.kerubo@gmail.com>
2023-09-07 18:20:02 -0700

    Program that prints a string using the printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints given string without using puts().
+ * Use printf() function.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
