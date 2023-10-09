1d026c744dd30cef728973851e27cc83cf613158
ShalyneKerubo <shalyne.kerubo@gmail.com>
2023-10-09 14:33:56 -0700

    A structure named 'dog' with given elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains details of a dog
+ * @name: first member; name of the dog
+ * @age: second member; age of the dog
+ * @owner: third member; owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
