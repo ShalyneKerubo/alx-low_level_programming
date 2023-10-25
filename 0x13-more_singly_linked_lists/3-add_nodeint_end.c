2246c87340ad2d6d11ff7dc8e526313dca3b056f
ShalyneKerubo <shalyne.kerubo@gmail.com>
2023-10-24 22:34:11 -0700

    Function that adds a new node at the end of a 'listint_t' list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * *add_nodeint_end - adds a new node at the end of a 'listint_t' list
+ * add_nodeint_end - adds a new node at the end of a 'listint_t' list
  * @head: double pointer to the head of the list
  * @n: constant integer
  *
